441841f32a8380d7c0f77a9c56e878f069b4a6b5
Psychox1k <success01001011@gmail.com>
2025-11-09 23:23:12 +0100

    feat(views): add base ModelViewSets for airport app

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airport.models import (
+    Flight,
+    AirplaneType,
+    Airport,
+    Route, Crew,
+    Order,
+    Ticket
+
+)
+from airport.serializers import (
+    FlightSerializer,
+    AirportSerializer,
+    AirplaneTypeSerializer,
+    RouteSerializer,
+    CrewSerializer,
+    OrderSerializer,
+    TicketSerializer
+)
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
