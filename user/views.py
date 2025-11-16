672d81c72121e5cdc753a29e76ad98b05e3baebf
Psychox1k <success01001011@gmail.com>
2025-11-16 21:59:30 +0100

    feat(user_app): implement views for user management

---
+++ user/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from user.serializers import UserSerializer
+
+
+# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
+
+    def get_object(self):
+        return self.request.user
